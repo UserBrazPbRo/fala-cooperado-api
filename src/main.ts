63c35b37b3ef8877d898684380bf2030979bf4f8
matheus braz <matheus.braz@gmail.com>
2025-04-05 06:45:54 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://front.falacooperado.com.br/*',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
   });
